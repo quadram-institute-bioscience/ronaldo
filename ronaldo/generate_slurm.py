58b7371a43629698322da5cc98505278bf319b24
Ubuntu <nabill@happykhan.com>
2020-09-08 12:42:58 +0100

    script for slurm scripts

---
+++ ronaldo/generate_slurm.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+"""
+generate_slurm makes submissions scripts, for a dir of results dirs 
+
+generate_slurm makes submissions scripts, for a dir of results dirs 
+
+### CHANGE LOG ### 
+2020-09-08 Nabil-Fareed Alikhan <nabil@happykhan.com>
+    * Initial build
+"""
+
+import time
+import os 
+import argparse
+import logging 
+from os import path
+import meta
+import sys 
+import re
+epi = "Licence: " + meta.__licence__ +  " by " +meta.__author__ + " <" +meta.__author_email__ + ">"
+logging.basicConfig()
+log = logging.getLogger()
+
+def main(args):
+    log.info(args)
+    for dir in os.listdir(args.data_dir):
+        illumina_read_path = path.join(args.data_dir, dir, 'ncovIllumina_sequenceAnalysis_readMapping/')
+        ont_read_path  = path.join(args.data_dir, dir, 'articNcovNanopore_sequenceAnalysisMedaka_articMinIONMedaka')
+        valid_read_path = None
+        if path.exists(illumina_read_path):
+            valid_read_path = illumina_read_path
+        elif path.exists(illumina_read_path):
+            valid_read_path = ont_read_path
+        runname_match = re.match('.+\.(20\d+).*', dir)
+        if runname_match and valid_read_path:
+            runname = runname_match.group(1)
+            blank_list = []
+            for bam_file in os.listdir(valid_read_path):
+                for blank_name in args.blank_prefix:                        
+                    if bam_file.lower().startswith(blank_name.lower()):
+                        blank_list.append(bam_file)
+                        break
+            write_pbs(args.output, runname, valid_read_path, blank_list ,args.ctdata)
+
+def write_pbs(output_dir, runname, datadir, blanks, ctdata):
+    output_script_path  = path.join(output_dir, f'ronaldo.{runname}.sh')
+    with open(output_script_path, 'w') as out_handle:
+        out_handle.write('#!/bin/bash\n')
+        out_handle.write('#SBATCH -p qib-long,nbi-short,nbi-medium,nbi-long\n')
+        out_handle.write('#SBATCH -t 0-2:00\n')
+        out_handle.write('#SBATCH -c 2\n')
+        out_handle.write(f'#SBATCH -J ronaldo.{runname}\n')
+        out_handle.write(f'#SBATCH -J ronaldo.{runname}\n')
+        out_handle.write('source python-3.7.2\n')
+        out_handle.write('source ~/ronaldo/venv/bin/activate\n')
+        out_handle.write('cd ~/ronaldo/\n')
+        blanks = ' '.join(blanks)
+        out_handle.write(f'python ronaldo/ronaldo.py calculate --ctdata {ctdata} {runname} {datadir} {blanks}\n')
+
+if __name__ == '__main__':
+    start_time = time.time()
+    log.setLevel(logging.INFO)
+    desc = __doc__.split('\n\n')[1].strip()
+    parser = argparse.ArgumentParser(description=desc,epilog=epi)
+    parser.add_argument ('-v', '--verbose', action='store_true', default=False, help='verbose output')
+    parser.add_argument('--version', action='version', version='%(prog)s ' + meta.__version__)
+    parser.add_argument('-o','--output',action='store',help='output directory', default='ronaldo_out')
+    parser.add_argument('data_dir', action='store', help='Dir with data dirs')
+    parser.add_argument('ctdata', action='store', help='ct_data file')
+    parser.add_argument('blank_prefix', metavar='N', nargs='+',  help='Prefix to find blanks')
+
+    args = parser.parse_args()
+    if args.verbose: 
+        log.setLevel(logging.DEBUG)
+        log.debug( "Executing @ %s\n"  %time.asctime())    
+    main(args)
+    if args.verbose: 
+        log.debug("Ended @ %s\n"  %time.asctime())
+        log.debug('total time in minutes: %d\n' %((time.time() - start_time) / 60.0))
+    sys.exit(0)
